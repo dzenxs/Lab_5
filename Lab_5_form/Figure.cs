86f65971c34620df6ad9c41f36d777113e972c87
Stovba Pavlo <stovbapavlo@gmail.com>
2023-10-31 19:20:59 +0200

    Add project files.

---
+++ Lab_5_form/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_form
+{
+    public abstract class Figure
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public abstract void DrawBlack();
+        public abstract void HideDrawingBackGround();
+        public abstract void MoveRight();
+
+        public Figure(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
---
+++ Lab_5_form/Square.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab_5_form
+{
+    public class Square : Figure
+    {
+        public int SideLength { get; set; }
+
+        public Square(int x, int y, int sideLength) : base(x, y)
+        {
+            SideLength = sideLength;
+        }
+        public override void DrawBlack()
+        {
+            Form activeForm = Form1.ActiveForm;
+            if (activeForm != null)
+            {
+                Graphics graphics = activeForm.CreateGraphics();
+                graphics.DrawRectangle(Pens.Black, X - SideLength / 2, Y - SideLength / 2, SideLength, SideLength);
+            }
+        }
+
+        public override void HideDrawingBackGround()
+        {
+            Form activeForm = Form1.ActiveForm;
+            if (activeForm != null)
+            {
+                Graphics graphics = activeForm.CreateGraphics();
+                graphics.DrawRectangle(new Pen(activeForm.BackColor), X - SideLength / 2, Y - SideLength / 2, SideLength, SideLength);
+            }
+        }
+
+        public override void MoveRight()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                DrawBlack();
+                System.Threading.Thread.Sleep(100);
+                HideDrawingBackGround();
+                X++;
+            }
+        }
+
+
+    }
+}
